4b151fcde3e9080183ab32a67ce41cb892b4688c
Bryan Alexis <122324403+bryanalexis24@users.noreply.github.com>
2023-09-28 12:24:18 -0400

    Split 9x9 board into 3x3 boxes

--- SudokuSolver.java
+++ SudokuSolver.java
@@ -33,7 +33,11 @@ public class SudokuSolver {
 
     private static void printBoard(int[][] board) {
         for (int i = 0; i < boardSize; i++) {
+            if (i % 3 == 0 && i != 0)
+                System.out.println("-----------");
             for (int j = 0; j < boardSize; j++) {
+                if (j % 3 == 0 && j != 0)
+                    System.out.print("|");
                 System.out.print(board[i][j]);
             }
             System.out.println();
